63c69576c4b15ea942252a2ff86312723da2954e
Jose B <jose.zdy@gmail.com>
2021-03-14 17:53:53 -0500

    reviviendo la cosa

--- base/base.ino
+++ base/base.ino
@@ -1,170 +1,162 @@
 
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <RF24Network.h>
+// #include <ESP8266WiFi.h>
+// #include <ESP8266HTTPClient.h>
+// #include <WiFiClient.h>
 #include <RF24.h>
+#include <RF24Network.h>
 #include <SPI.h>
 
 #define CE_PIN 2
-#define CSN_PIN 15
+#define CSN_PIN 4
 
-const char* ssid = "GATOS";
-const char* password = "venceromorir";
-const int timeToCollect = 10000;
-const int period = 10000;
-unsigned long timeNow = 0;
-const uint16_t gnomos[2] = { 01, 02 };
-String humidities[] = {};
-String crops[2] = {"albahaca", "besitos"};
+#define NETWORK_CHANNEL 90
+#define BASE_ID 00
+
+#define PERIOD 1000
+
+// const char* ssid = "GATOS";
+// const char* password = "venceromorir";
+// const int timeToCollect = 10000;
+unsigned long last_millis = millis();
+const uint16_t gnomos[2] = {01, 02};
+// String humidities[] = {};
+// String crops[2] = {"albahaca", "besitos"};
 
 RF24 radio(CE_PIN, CSN_PIN);
-
 RF24Network network(radio);
 
-const uint16_t this_node = 00;
-const uint16_t other_node = 01;
 
-unsigned long last_sent;
-unsigned long packets_sent;
+// unsigned long packets_sent;
 
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
+// struct payload_t {
+//   unsigned long ms;
+//   unsigned long counter;
+// };
 
-String payload = "";
+// String payload = "";
 
-void setup() 
-{
-  Serial.begin(115200);
-  InitializeRadio();
-  InitializeWiFi();
-}
- 
-void loop()
-{
-  network.update();
-  
-  CollectData();
+void setup() {
+    Serial.begin(115200);
+    initRadio();
+    // InitializeWiFi();
 }
 
-void InitializeRadio()
-{
-  SPI.begin();
-  radio.begin();
-  network.begin(90, this_node);
+void loop() {
+    network.update();
+
+    if (millis() - last_millis > PERIOD) {
+        requestData();
+        last_millis = millis();
+    }
+
+    // network.update();
+
+    if (network.available()) {
+        RF24NetworkHeader header;
+        uint16_t payloadSize = network.peek(header);
+        char payload[payloadSize];
+
+        network.read(header, &payload, payloadSize);
+
+        String msg = String(payload).substring(0, payloadSize);
+        Serial.print("Got: ");
+        Serial.println(msg);
+
+        // SendMessage( msg );
+    }
 }
 
-void InitializeWiFi()
-{
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("\nConnecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    Serial.print(".");
-    delay(1000);
-  }
+void requestData() {
+    Serial.print("---------- ");
+    Serial.println(millis());
+    for (int gnomo = 0; gnomo < 2; gnomo++) {
+        Serial.print("Reaching gnomo ");
+        Serial.print(gnomos[gnomo]);
+
+        RF24NetworkHeader header(gnomos[gnomo]);
+        const char* payload = "DATA_COLLECT";
+        bool ok = network.write(header, payload, strlen(payload));
+
+        if (ok)
+            Serial.println("... OK");
+        else
+            Serial.println("... failed");
+    }
 }
 
-void CollectData()
-{
-  if( millis() >= timeNow + period )
-  {
-      timeNow += period;
-
-      RequestData();
-  }
-
-  network.update();
-  
-  if( network.available() )
-  {  
-    RF24NetworkHeader header;
-    uint16_t payloadSize = network.peek(header);
-    char payload[payloadSize];
-    
-    network.read(header,&payload,payloadSize);
-        
-    String msg = String(payload).substring(0,payloadSize);
-    Serial.print("msg recibido de base>");
-    Serial.println(msg);
-
-    SendMessage( msg );
-  }
-}
-
-void SendMessage(String humidity)
-{
-    String message = "gnomo_0,cultivo=maiz humidity=";
-    message.concat(humidity);
-
-    Serial.print("Influx query: "); Serial.println(message);
-    SendDataToServer(message);  
-}
-
-void SendDataToServer( String message )
-{
-      if (WiFi.status() == WL_CONNECTED) 
-      {
-        WiFiClient client;
-        HTTPClient http;
-    
-        Serial.print("[HTTP] begin...\n");
-        if (http.begin(client, "http://tucanoar.com:8086/write?db=tucanodb")) {  // HTTP
-    
-          Serial.print("[HTTP] POST...\n");
-          // start connection and send HTTP header
-          
-          Serial.println(message);
-          int httpCode = http.POST( message );
-    
-          // httpCode will be negative on error
-          if (httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-    
-            // file found at server
-            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-              String payload = http.getString();
-              Serial.println(payload);
-            }
-          } else {
-            Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-          }
-    
-          http.end();
-        } else {
-          Serial.printf("[HTTP} Unable to connect\n");
+void initRadio() {
+    SPI.begin();
+    if (!radio.begin()) {
+        Serial.println("Radio hardware not responding!");
+        while (true) {
+            // do nothing
         }
-      }
+    }
+    network.begin(NETWORK_CHANNEL, BASE_ID);
 }
 
-bool MessageWasReceived(int gnomoId)
-{
-  return false;
-}
+// void InitializeWiFi()
+// {
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(ssid, password);
+//   Serial.println("\nConnecting to WiFi");
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     Serial.print(".");
+//     delay(1000);
+//   }
+// }
 
-void RequestData()
-{  
-  for( int gnomo = 0; gnomo < 2; gnomo++ )
-  {    
-    Serial.print("Sending...");
-    Serial.print("gnomo");
-    Serial.print(gnomos[gnomo]);
-    //payload_t payload = { millis(), packets_sent++ };
-    char *payload = "DATA_COLLECT";
-    RF24NetworkHeader header(gnomos[gnomo]);
-    Serial.println(strlen(payload));
-    bool ok = network.write(header,payload,strlen(payload));
-    
-    if (ok)
-      Serial.println(" ok.");
-    else
-      Serial.println(" failed."); 
-    
-//    Serial.print("Sent: ");
-//    Serial.println(gnomos[gnomo]);  
-  }
-}
+// void SendMessage(String humidity)
+// {
+//     String message = "gnomo_0,cultivo=maiz humidity=";
+//     message.concat(humidity);
+
+//     Serial.print("Influx query: "); Serial.println(message);
+//     SendDataToServer(message);
+// }
+
+// void SendDataToServer( String message )
+// {
+//       if (WiFi.status() == WL_CONNECTED)
+//       {
+//         WiFiClient client;
+//         HTTPClient http;
+
+//         Serial.print("[HTTP] begin...\n");
+//         if (http.begin(client, "http://tucanoar.com:8086/write?db=tucanodb"))
+//         {  // HTTP
+
+//           Serial.print("[HTTP] POST...\n");
+//           // start connection and send HTTP header
+
+//           Serial.println(message);
+//           int httpCode = http.POST( message );
+
+//           // httpCode will be negative on error
+//           if (httpCode > 0) {
+//             // HTTP header has been send and Server response header has been
+//             handled Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+//             // file found at server
+//             if (httpCode == HTTP_CODE_OK || httpCode ==
+//             HTTP_CODE_MOVED_PERMANENTLY) {
+//               String payload = http.getString();
+//               Serial.println(payload);
+//             }
+//           } else {
+//             Serial.printf("[HTTP] POST... failed, error: %s\n",
+//             http.errorToString(httpCode).c_str());
+//           }
+
+//           http.end();
+//         } else {
+//           Serial.printf("[HTTP} Unable to connect\n");
+//         }
+//       }
+// }
+
+// bool MessageWasReceived(int gnomoId)
+// {
+//   return false;
+// }
--- gnomo/gnomo.ino
+++ gnomo/gnomo.ino
@@ -1,116 +1,112 @@
-#include "gnomoX.h"
-#include <RH_NRF24.h>
+// #include "gnomoX.h"
+// #include <RH_NRF24.h>
 
-#include <RF24Network.h>
 #include <RF24.h>
+#include <RF24Network.h>
 #include <SPI.h>
 
-#define GNOMO_ID "gnomo1"
-#define HIGH_LIMIT 1023
-#define LOW_LIMIT 500
+#define CE_PIN 2
+#define CSN_PIN 4
 
-const int humidityInPin = A0;
-const String dataRequested = "DATA_COLLECT";
-const char emptyMessage = '0';
+#define NETWORK_CHANNEL 90
+#define BASE_ID 00
+#define GNOMO_ID 01
 
-RF24 radio(8,10);
+#define ANALOG_PIN A0
+#define ANALOG_HIGH 1023
+#define ANALOG_LOW 500
 
+
+RF24 radio(CE_PIN, CSN_PIN);
 RF24Network network(radio);
-const uint16_t this_node = 02;
-const uint16_t baseId = 00;
-const uint16_t gnomos[2] = { 00, 01 };
 
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
+// struct payload_t {
+//     unsigned long ms;
+//     unsigned long counter;
+// };
 
-void setup() 
-{
-  Serial.begin(115200);
-  InitializeRadio(); 
+void setup() {
+    Serial.begin(115200);
+    initRadio();
+    Serial.println("All good!");
 }
- 
-void loop()
-{  
-  network.update();
-  
-  if( network.available() )
-  {  
-    RF24NetworkHeader header;
-    uint16_t payloadSize = network.peek(header);
-    char payload[payloadSize];
-    
-    network.read(header,&payload,payloadSize);
-        
-    String msg = String(payload).substring(0,payloadSize);
-    Serial.print("substring>");
-    Serial.println(msg);
-    
-    if( msg == dataRequested )
-    {
-      char buff[16];
-      int hum = GetHumidity();
-      
-      itoa(hum, buff, 10);
-      Serial.print("Hum: "); Serial.println(hum);
-      Serial.print("Buff: "); Serial.println(buff);
-      SendMessage(buff);
+
+void loop() {
+    String msg;
+    network.update();
+
+    while (network.available()) {
+        RF24NetworkHeader header;
+        const uint16_t payloadSize = network.peek(header);
+        char payload[payloadSize];
+
+        network.read(header, &payload, payloadSize);
+
+        msg = String(payload).substring(0, payloadSize);
+        Serial.print("Gnomo got: ");
+        Serial.println(msg);
     }
-  }
+
+    delay(10);
+    if (msg == "DATA_COLLECT") {
+        char buff[16];
+        int hum = getHumidity();
+
+        itoa(hum, buff, 10);
+        // Serial.print("Hum: ");
+        // Serial.println(hum);
+        // Serial.print("Buff: ");
+        // Serial.println(buff);
+        sendMessage(buff);
+    }
+
 }
 
-void InitializeRadio()
-{
-  SPI.begin();
-  radio.begin();
-  network.begin(90, this_node);
+void initRadio() {
+    SPI.begin();
+    if (!radio.begin()) {
+        Serial.println("Radio hardware not responding!");
+        while (true) {
+            // do nothing
+        }
+    }
+    network.begin(NETWORK_CHANNEL, GNOMO_ID);
 }
 
-int GetHumidity()
-{
-  int sensorValue = analogRead(humidityInPin);
-  int outputValue = map(sensorValue, LOW_LIMIT, HIGH_LIMIT, 100, 0);
-
-  // Serial.print("Sensor Value: "); Serial.println(sensorValue);
-
-  return outputValue;
+int getHumidity() {
+    return 8963;
+    // return map(analogRead(ANALOG_PIN), ANALOG_LOW, ANALOG_HIGH, 100, 0);
 }
 
-void SendMessage(char* payload )
-{
-//  String message = GNOMO_ID;
-//  message.concat( String(humidity) );
-//  
-//  uint8_t dataArray[message.length()+1];
-//  message.getBytes(dataArray,message.length()+1);
-//  
-//  Serial.print("Sent: ");
-//  Serial.println(message);
+void sendMessage(const char* payload) {
+    Serial.print("--------- ");
+    Serial.println(millis());
+    Serial.print("Reaching base ");
+    Serial.print(BASE_ID);
+    Serial.print(": ");
+    Serial.print(payload);
 
-    RF24NetworkHeader header(baseId);
-    bool ok = network.write(header,payload,strlen(payload));
-    
+    RF24NetworkHeader header(BASE_ID);
+    bool ok = network.write(header, payload, strlen(payload));
+
     if (ok)
-      Serial.println("message sent");
+        Serial.println("... OK");
     else
-      Serial.println("message dalivery failed");
+        Serial.println("... failed");
 }
 
-char MessageReceived()
-{
-  while ( network.available() ) 
-  {  
-    RF24NetworkHeader header;
-    payload_t payload;
-    network.read(header,&payload,sizeof(payload));
-    Serial.print("Received packet #");
-    Serial.print(payload.counter);
-    Serial.print(" at ");
-    Serial.println(payload.ms);
+// char MessageReceived() {
+//     while (network.available()) {
+//         RF24NetworkHeader header;
+//         payload_t payload;
+//         network.read(header, &payload, sizeof(payload));
+//         Serial.print("Received packet #");
+//         Serial.print(payload.counter);
+//         Serial.print(" at ");
+//         Serial.println(payload.ms);
 
-    return payload.ms;
-  }
-  
-  return emptyMessage;
-}
+//         return payload.ms;
+//     }
+
+//     return emptyMessage;
+// }
